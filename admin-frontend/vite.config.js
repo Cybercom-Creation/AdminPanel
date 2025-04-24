337c4bf91d675b2c60309ad382158d45e7f2deb9
Devansh Sevak <devanshsevak38@gmail.com>
2025-04-24 09:58:55 +0530

    Update vite.config.js

--- admin-frontend/vite.config.js
+++ admin-frontend/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/a3b7x9z/', // <-- Add this line
+  // base: '/a3b7x9z/', // <-- Add this line
+  base: './', // <-- Add this line
 })
